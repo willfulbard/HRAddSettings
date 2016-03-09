d77b50ccbc9fc7abe3249715df37b2ee3af79316
Hack Reactor Students <accounts@hackreactor.com>
2016-03-09 09:56:03 -0800

    Setting work better now

--- install.sh
+++ install.sh
@@ -1,25 +1,20 @@
 #/bin/bash
 
 #makes some of my settings work with HR settings
-ln -s ~/dotfiles ~/HRAddSettings
+ln -s ~/HRAddSettings ~/dotfiles
 
 cp ~/.gitconfig ~/.gitconfig.hrorig
-cat gitconfig >> ~/.gitconfig
+cat ~/HRAddSettings/gitconfig >> ~/.gitconfig
 
 cp ~/.zshrc ~/.zshrc.hrorig
-cat zshrc >> ~/.zshrc
+cat ~/HRAddSettings/zshrc >> ~/.zshrc
 
-brew install macvim --with-override-system-vim
 brew install cmake
-brew install the_silver_searcher
 
 mkdir ~/.vim
 mkdir ~/.vim-swp
-ln -s ~/.vimrc vim/vimrc
+ln -s ~/HRAddSettings/vim/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa
-
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --tern-completer 
 
