8332cc6e0d5daedf59faa61546e75ee6d0de513b
Hack Reactor Students <accounts@hackreactor.com>
2016-03-09 11:09:28 -0800

    Further refine the install script

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 This script will add some settings that I personally like to use in workstations to an HR workstation (git aliases, vim settings, cmd line aliases, soft links, etc). This is a non-destructive script, copying any file it intends to edit to the same directory with a .hrorig extention.
 
-Run the `install.sh` to add the settings.
+# Installation
+1) run `git clone https://github.com/willfulbard/HRAddSettings.git` in the machine's home directory
+2) run `chmod 775 ~/HRAddSettings/install.sh`
+3) Run the `~/HRAddSettings/install.sh` to add the settings.
--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #/bin/bash
 
 #makes some of my settings work with HR settings
+rm -f ~/dotfiles
 ln -s ~/HRAddSettings ~/dotfiles
 
-cp ~/.gitconfig ~/.gitconfig.hrorig
-cat ~/HRAddSettings/gitconfig >> ~/.gitconfig
-
-cp ~/.zshrc ~/.zshrc.hrorig
-cat ~/HRAddSettings/zshrc >> ~/.zshrc
+cp -n ~/.gitconfig ~/.gitconfig.hrorig
+cat ~/HRAddSettings/gitconfig.addsettings >> ~/.gitconfig
 
 brew install cmake
 
 mkdir ~/.vim
 mkdir ~/.vim-swp
+rm -f ~/.vimrc
 ln -s ~/HRAddSettings/vim/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa
+
+source ~/HRAddSettings/zshrc.addsettings
 
